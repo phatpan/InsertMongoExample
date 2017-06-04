26c9e6e46c9a320e40629024503d6daf89ef79d9
phatpan <phat.pan@gmail.com>
2017-06-04 20:43:48 +0700

    insert data in mongodb

--- src/main.go
+++ src/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main(){
+import (
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
+func main(){
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	
+	collection := session.DB("income").C("users")
+	err = collection.Insert(bson.M{"name": "Phatcharaphan"})
+	if err != nil {
+		panic(err)
+	}
 }
